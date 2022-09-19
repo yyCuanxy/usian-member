a720581d7d3f817cd19c6007b7ab69ab0226fc12
yyCuanxy <2822843586@qq.com>
2022-09-20 00:39:50 +0800

    feat: 配置标题、ico图标、serve

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    // 端口号
+    port: 8888,
+    // 主机名
+    host: "localhost",
+    // 是否自动打开浏览器
+    open: false,
+    // 是否开启https
+    https: false,
+  },
+  // 关闭eslint
+  lintOnSave: false,
 });
